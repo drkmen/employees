f608d8014294da0ea45a7653d33ce35d5decbd89
polyakovigor <igor26.polyakov@gmail.com>
2018-06-05 11:05:50 +0300

    Add search feature

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,35 +1,50 @@
 # frozen_string_literal: true
+
 # scope :role, -> (role) { where role: role }
 # scope :office, -> (office) { where office: office }
 # scope :department, -> (department) { where department: department }
+# scope :deleted, -> { unscoped.where(deleted: true) }
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-
-  before do
-    Employee.new(first_name: 'qwe', last_name: 'qweqwe', email: 'email@gmail.com', password: '12345678').save
-    Employee.new(first_name: '2qwe', last_name: '2qweqwe', email: '2email@gmail.com', password: '12345678').save
-  end
+  let(:employee) { create :employee, first_name: 'John', last_name: 'Wick', email: 'wick@gmail.com', password: '123456' }
+  let(:employee_deleted) { create :employee, first_name: 'Joe', last_name: 'Doe', email: 'doe@gmail.com', password: '123456' }
 
   describe 'scope tests' do
-    it 'default scope test' do
-      expect(Employee.all.size).to be(2)
-      expect(Employee.deleted.size).to be(0)
+    it 'default scope' do
+      expect(Employee.all).to eq [employee, employee_deleted]
+      expect(Employee.deleted).to eq []
     end
 
-    it 'delete method test' do
-      Employee.last.delete
-      expect(Employee.all.size).to be(1)
-      expect(Employee.deleted.size).to be(1)
+    it 'deleted scope' do
+      employee_deleted.delete
+      expect(Employee.deleted).to eq [employee_deleted]
     end
-
-    it 'restore method test' do
-      Employee.last.delete
-      Employee.deleted.last.restore
-      expect(Employee.all.size).to be(2)
-      expect(Employee.deleted.size).to be(0)
-    end
-
   end
 
+  describe 'methods' do
+    it '#name method' do
+      expect(employee.name).to eq 'John Wick'
+    end
+
+    it '#delete method' do
+      employee.delete
+      expect(employee.deleted).to be true
+    end
+
+    it '#restore method' do
+      employee_deleted.restore
+      expect(employee_deleted.deleted).to be false
+    end
+
+    it '#search method by first_name' do
+      result = Employee.search('John')
+      expect(result).to eq [employee]
+    end
+
+    it '#search method by first_name & last_name' do
+      result = Employee.search('Joe Doe')
+      expect(result).to eq [employee_deleted]
+    end
+  end
 end

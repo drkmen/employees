968c982d004ec638222f73d10a6eaf62ecde6f13
Anton <capitalsav@gmail.com>
2018-06-06 11:27:22 +0300

    Add skill type to model skill and test it

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,4 +4,5 @@ class Skill < ApplicationRecord
   has_many :projects, through: :resource_skills
 
   validates :name, presence: true, uniqueness: true
+  enum skill_type: { other_skill: 0, programming_language: 1, library: 2, framework: 3, service: 4, database: 5 }
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @other_skill = Skill.create(name: 'git', skill_type: 'other_skill')
+    @programming_language_skill = Skill.create(name: 'javascript', skill_type: 'programming_language')
+    @library_skill = Skill.create(name: 'jquery', skill_type: 'library')
+    @framework_skill = Skill.create(name: 'angularjs', skill_type: 'framework')
+    @database_skill = Skill.create(name: 'postgresql', skill_type: 'database')
+  end
+
+  describe 'enum skill_type' do
+    it 'skill other_skill have correct skill_type value' do
+      expect(@other_skill.other_skill?).to be(true)
+      expect(@other_skill.framework?).to be(false)
+    end
+
+    it 'skill other_skill have correct skill_type value' do
+      expect(@programming_language_skill.programming_language?).to be(true)
+      expect(@programming_language_skill.framework?).to be(false)
+    end
+
+    it 'skill other_skill have correct skill_type value' do
+      expect(@library_skill.library?).to be(true)
+      expect(@library_skill.framework?).to be(false)
+    end
+
+    it 'skill other_skill have correct skill_type value' do
+      expect(@framework_skill.framework?).to be(true)
+      expect(@framework_skill.other_skill?).to be(false)
+    end
+
+    it 'skill other_skill have correct skill_type value' do
+      expect(@database_skill.database?).to be(true)
+      expect(@framework_skill.other_skill?).to be(false)
+    end
+  end
 end

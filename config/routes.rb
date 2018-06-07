8b743c6276628c8e678ca88a5ba5d4e4e813ef10
Sergey Dedik <mr.serg.vit@gmail.com>
2018-06-07 16:54:34 +0300

    added migration for move expeirience

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   devise_for :employees, controllers: { sessions: 'sessions', invitations: 'invitations' }
 
-  resources :employees
+  resources :employees do
+    patch :skill_experience
+  end
   resources :projects, except: %i[show index]
   resources :skills, except: %i[show index]
   resources :archive, only: %i[index destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_121148) do
+ActiveRecord::Schema.define(version: 2018_06_07_135250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,11 +96,11 @@ ActiveRecord::Schema.define(version: 2018_06_07_121148) do
     t.integer "skill_id"
     t.integer "project_id"
     t.integer "employee_id"
+    t.string "experience"
   end
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
-    t.string "experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "skill_type", default: 0, null: false

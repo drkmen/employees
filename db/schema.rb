bbbe68f6e4b482562e25f0f14af600d8fee2a70f
vladfreel <vladfreelmaster@gmail.com>
2018-06-07 15:13:21 +0300

    Add link to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_072730) do
+ActiveRecord::Schema.define(version: 2018_06_07_121148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2018_06_06_072730) do
     t.bigint "employee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
     t.index ["employee_id"], name: "index_projects_on_employee_id"
   end
 

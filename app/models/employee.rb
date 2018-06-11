b8f80d477e1c95924dd4a032b19afb361e679421
vladfreel <vladfreelmaster@gmail.com>
2018-06-11 11:28:45 +0300

    Add has_many skills and migration

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,6 +6,7 @@ class Employee < ApplicationRecord
   has_many :resource_skills
   has_many :skills, through: :resource_skills
   has_many :projects
+  has_many :own_skills, -> { where(employee_id: self.id) }, class_name: 'Skill'
 
   friendly_id :friendly_name, use: :slugged
 

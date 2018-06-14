d16cccef76c5ad73cfe08baeba4e3a00a3f76fcc
polyakovigor <igor26.polyakov@gmail.com>
2018-06-14 17:24:19 +0300

    Add redirect to root_path after p/w

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,28 @@
 class ApplicationController < ActionController::Base
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_employee_location!, if: :storable_location?
 
   protected
 
-  def after_sign_in_path_for(resource)
-    URI(request.referer).path || root_path
+  def after_sign_out_path_for(resource_or_scope)
+    URI.parse(request.referer).path || root_path
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:invite, keys: %i[first_name last_name role])
+    devise_parameter_sanitizer.permit(:invite, keys: %i[first_name
+                                                        last_name
+                                                        role])
+  end
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? &&
+      !devise_controller? && !request.xhr?
+  end
+
+  def store_employee_location!
+    store_location_for(:employee, request.fullpath)
   end
 end

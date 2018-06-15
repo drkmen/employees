a8f978da479ed781d4ffc8655c82526bdeb30cb9
Sergey Dedik <mr.serg.vit@gmail.com>
2018-06-15 16:28:58 +0300

    first attempt to add errors body

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,25 @@
 
 # Sessions Controller
 class SessionsController < Devise::SessionsController
-  respond_to :html, :json
+  respond_to :html, :json, :js
+
+  def create
+    @employee = Employee.find_by(email: params[:employee][:email])
+    if @employee
+      if @employee.valid_password?(params[:employee][:password])
+      else
+        respond_to do |format|
+          format.html { render}
+          format.js { render}
+          format.json {render }
+        end
+      end
+    else
+      respond_to do |format|
+        format.html { render}
+        format.js { render}
+        format.json {render }
+      end
+    end
+  end
 end

d814c755c899a9e44213e1764ad28343eca82435
drkmen <drkmen@rambler.ru>
2018-06-18 12:45:36 +0300

    forkflow fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :store_employee_location!, if: :storable_location?
+  before_action :authenticate_employee!
 
   protected
 
@@ -13,9 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:invite, keys: %i[first_name
-                                                        last_name
-                                                        role])
+    devise_parameter_sanitizer.permit(:invite, keys: %i[first_name last_name role])
   end
 
   private
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,21 @@
 class SessionsController < Devise::SessionsController
 
   def create
-    @referer = request.referer || root_path
-    self.resource = warden.authenticate(auth_options)
-    if resource && resource.active_for_authentication?
-      sign_in(resource_name, resource)
-    else
-      @errors = 'Wrong credentials'
-    end
+    # @referer = request.referer || root_path
+    # self.resource = warden.authenticate(auth_options)
+    # if resource && resource.active_for_authentication?
+    #   sign_in(resource_name, resource)
+    #   respond_to do |format|
+    #     format.html { redirect_to root_path }
+    #     format.js
+    #   end
+    # else
+    #   @errors = 'Wrong credentials'
+    #   respond_to do |format|
+    #     format.html { redirect_to new_employee_session_path }
+    #     format.js
+    #   end
+    # end
+    super
   end
 end

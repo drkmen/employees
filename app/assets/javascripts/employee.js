ef4ea8c8e3c5277e4226fd150fde3c5d9f3eb903
Sergey Dedik <mr.serg.vit@gmail.com>
2018-07-12 18:18:33 +0300

    rewrited pdf partial & added dynamic generation for a pdf

--- app/assets/javascripts/employee.js
+++ app/assets/javascripts/employee.js
@@ -9,6 +9,47 @@ $( document ).on('turbolinks:load', function() {
         $section.find('.content').toggleClass('d-none');
         $(this).toggleClass('fa-long-arrow-left').toggleClass('fa-long-arrow-right');
     });
+
+    const $checkAll = $('#pdfEmployeeAll');
+    const $linkToPdf = $('#linkToPdf');
+    const $allCheckboxesAtModal = $('#pdfModal .fields input[type=checkbox]');
+    const baseHrefToPdf = $linkToPdf.attr('href');
+
+    $checkAll.on('change', function(){
+        const isChecked = this.checked;
+        $allCheckboxesAtModal.each(function(index, element){
+            element.checked = isChecked;
+        });
+        generateNewUrl()
+    });
+
+    $allCheckboxesAtModal.on('change', function(){
+        let allChecked = true;
+        for (let i = 0; i < $allCheckboxesAtModal.length; i++) {
+            allChecked = $allCheckboxesAtModal[i].checked;
+            if (!allChecked){
+                break;
+            }
+        }
+        $checkAll.prop('checked', allChecked);
+        generateNewUrl()
+    });
+
+    function generateNewUrl() {
+        let newUrlToPdf = baseHrefToPdf;
+        let wasAddedFirst = false;
+        for (let i = 0; i < $allCheckboxesAtModal.length; i++) {
+            let tempElement = $allCheckboxesAtModal[i];
+            if (wasAddedFirst){
+                newUrlToPdf += '&';
+            } else {
+                newUrlToPdf += '?';
+                wasAddedFirst = true;
+            }
+            newUrlToPdf += tempElement.id + '=' + tempElement.checked;
+        }
+        $linkToPdf.attr('href', newUrlToPdf);
+    }
 });
 
 function addFields(e) {
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,16 +13,17 @@ class EmployeesController < ApplicationController
     @employee.build_image unless @employee.image
     @project = @employee.projects.new
     @project.build_image
-
+    
     respond_to do |format|
       format.html
       format.pdf do
         render pdf: "#{@employee.slug}",
                template: 'employees/show_pdf.html.haml',
+               locals: { keys: params.permit(params.keys).to_h },
                layout: 'pdf.html.haml',
                encoding: 'UTF-8',
-               show_as_html: params.key?('debug')
-               # margin: { top: 25, bottom: 25 }
+               show_as_html: params.key?('debug'),
+               margin: { top: 15, bottom: 15, left: 20, right: 20 }
       end
     end
   end

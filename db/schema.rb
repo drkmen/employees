3e7d08a00e49e252a02342b15eda09d9df7e9132
Sergey Dedik <mr.serg.vit@gmail.com>
2018-08-20 13:45:19 +0300

    added repo link & minor fixed UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_083655) do
+ActiveRecord::Schema.define(version: 2018_08_20_081350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2018_06_21_083655) do
     t.string "link"
     t.boolean "active", default: false
     t.integer "manager_id"
+    t.string "repository"
     t.index ["employee_id"], name: "index_projects_on_employee_id"
   end
 

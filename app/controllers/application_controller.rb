5e90fa70f319e1c688c624f2dc13aad645ee9539
Sergey Dedik <mr.serg.vit@gmail.com>
2018-11-15 16:52:09 +0200

    fixed troubles with URI parse

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,8 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     URI.parse(request.referer).path || root_path
+  rescue => _
+    root_path
   end
 
   def configure_permitted_parameters

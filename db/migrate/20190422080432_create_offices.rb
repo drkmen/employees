750fcece61a116d45c121a36da217337f6fc5028
drkmen <drkmen@rambler.ru>
2019-04-22 14:11:35 +0300

    offices moved to DB, UI improvements

--- db/migrate/20190422080432_create_offices.rb
+++ db/migrate/20190422080432_create_offices.rb
@@ -3,6 +3,7 @@ class CreateOffices < ActiveRecord::Migration[5.2]
     create_table :offices do |t|
       t.string :name
       t.integer :department_id
+      t.integer :employees_count, default: 0
 
       t.timestamps
     end
